39cddc24b993b84a0826bbb148bd071eff9f0ff2
Zaqueo Echeverria <zaqueoae@gmail.com>
2023-10-09 12:40:52 +0200

    Update inicio.sh

--- inicio.sh
+++ inicio.sh
@@ -62,51 +62,95 @@ config_ssh () {
     echo 'PreferredAuthentications=publickey' >> ~/.ssh/config
 }
 
-printf "\n${BLUE}========================Creado los archivos config ssh========================${ENDCOLOR}\n"
-config_ssh
-printf "${GREEN}========================¡Archivos config ssh creados!========================${ENDCOLOR}\n"
 
-
-if ! (github-authenticated githubssh); then
-  ssh-keygen -b 4096 -t rsa -f ~/.ssh/id_rsagithub -q -N ""
-  chmod 400 ~/.ssh/id_rsagithub
-  chmod 644 ~/.ssh/id_rsagithub.pub
-  
-  rm -f ~/.ssh/github
-  touch ~/.ssh/github
-  echo 'Host githubssh' >> ~/.ssh/github
-  echo '        User git' >> ~/.ssh/github
-  echo '        HostName github.com' >> ~/.ssh/github
-  echo '        IdentityFile ~/.ssh/id_rsagithub' >> ~/.ssh/github
-  
-  #Añado las llaves a ssh agent
-  eval "$(ssh-agent)"
-  ssh-add ~/.ssh/id_rsagithub
-  pub=$(cat ~/.ssh/id_rsagithub.pub)
-  echo ''
-  echo ''
-  for (( ; ; ))
-  do
-      githubuser=0
-      githubpass=0
-      read -r -p "Escribe tu usuario de github: " githubuser
-      echo "Tu usuario de github es $githubuser"
+conexion_shh_github_bash_catinfog () {
+    if ! (github-authenticated githubssh); then
+      ssh-keygen -b 4096 -t rsa -f ~/.ssh/id_rsagithub -q -N ""
+      chmod 400 ~/.ssh/id_rsagithub
+      chmod 644 ~/.ssh/id_rsagithub.pub
+      
+      rm -f ~/.ssh/github
+      touch ~/.ssh/github
       echo ''
-      read -r -p "Escribe la api-key de $githubuser: " -s githubpass
+      echo 'Host githubssh' >> ~/.ssh/github
+      echo '        User git' >> ~/.ssh/github
+      echo '        HostName github.com' >> ~/.ssh/github
+      echo '        IdentityFile ~/.ssh/id_rsagithub' >> ~/.ssh/github
+      
+      #Añado las llaves a ssh agent
+      eval "$(ssh-agent)"
+      ssh-add ~/.ssh/id_rsagithub
+      pub=$(cat ~/.ssh/id_rsagithub.pub)
       echo ''
-      curl -u "$githubuser:$githubpass" -X POST -d "{\"title\":\"`hostname`\",\"key\":\"$pub\"}" https://api.github.com/user/keys
-      if github-authenticated githubssh; then
-          echo "Hemos conectado"
-          break
-      else
-          echo "Algo ha fallado: el nombre de usuario o el api token."
-          echo "Aquí tienes un manual para crear un api token: https://docs.github.com/es/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens"
-          read -n 1 -s -r -p "Pulsa Enter para volver a intentar conectar"
+      echo ''
+      for (( ; ; ))
+      do
+          githubuser=0
+          githubpass=0
+          read -r -p "Escribe tu usuario de github para conectar con el repo de Bash Catinfog: " githubuser
+          echo "Tu usuario de github es $githubuser"
           echo ''
-      fi
-  done
-fi
+          read -r -p "Escribe la api-key de $githubuser: " -s githubpass
+          echo ''
+          curl -u "$githubuser:$githubpass" -X POST -d "{\"title\":\"`hostname`\",\"key\":\"$pub\"}" https://api.github.com/user/keys
+          if github-authenticated githubssh; then
+              echo "Hemos conectado"
+              break
+          else
+              echo "Algo ha fallado: el nombre de usuario o el api token."
+              echo "Aquí tienes un manual para crear un api token: https://docs.github.com/es/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens"
+              read -n 1 -s -r -p "Pulsa Enter para volver a intentar conectar"
+              echo ''
+          fi
+      done
+    fi
+}
 
+conexion_shh_github_swap () {
+    if ! (github-authenticated swap); then
+      ssh-keygen -b 4096 -t rsa -f ~/.ssh/id_swap -q -N ""
+      chmod 400 ~/.ssh/id_swap
+      chmod 644 ~/.ssh/id_swap.pub
+      
+      rm -f ~/.ssh/github
+      touch ~/.ssh/github
+      echo ''
+      echo 'Host swap' >> ~/.ssh/github
+      echo '        User git' >> ~/.ssh/github
+      echo '        HostName github.com' >> ~/.ssh/github
+      echo '        IdentityFile ~/.ssh/id_swap' >> ~/.ssh/github
+      
+      #Añado las llaves a ssh agent
+      eval "$(ssh-agent)"
+      ssh-add ~/.ssh/id_swap
+      pub=$(cat ~/.ssh/id_rsagithub.pub)
+      echo ''
+      echo ''
+      for (( ; ; ))
+      do
+          githubuser=0
+          githubpass=0
+          read -r -p "Escribe tu usuario de github pra conectar con el repo de SWAP: " githubuser
+          echo "Tu usuario de github es $githubuser"
+          echo ''
+          read -r -p "Escribe la api-key de $githubuser: " -s githubpass
+          echo ''
+          curl -u "$githubuser:$githubpass" -X POST -d "{\"title\":\"`hostname`\",\"key\":\"$pub\"}" https://api.github.com/user/keys
+          if github-authenticated githubssh; then
+              echo "Hemos conectado"
+              break
+          else
+              echo "Algo ha fallado: el nombre de usuario o el api token."
+              echo "Aquí tienes un manual para crear un api token: https://docs.github.com/es/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens"
+              read -n 1 -s -r -p "Pulsa Enter para volver a intentar conectar"
+              echo ''
+          fi
+      done
+    fi
+}
+
+
+clonacion_bash_catinfog () {
 rm -rf ~/swap
 rm -rf ~/bash
 mkdir -p ~/swap
@@ -114,4 +158,20 @@ mkdir -p ~/bash
 git clone githubssh:zaqueoae/bashcatinfog.git ~/swap
 cp -rfp ~/swap/0-Caja_de_herramientas/* ~/bash/
 rm -rf ~/swap
+}
+
+printf "\n${BLUE}======================== Creando los archivos config ssh ========================${ENDCOLOR}\n"
+config_ssh
+printf "${GREEN}======================== ¡Archivos config ssh creados! ========================${ENDCOLOR}\n"
+
+printf "\n${BLUE}======================== Creando conexión con Github ========================${ENDCOLOR}\n"
+conexion_shh_github_bash_catinfog
+conexion_shh_github_swap
+printf "${GREEN}======================== ¡Conexión con Github creada! ========================${ENDCOLOR}\n"
+
+printf "\n${BLUE}======================== Clonando archivos bash catinfog ========================${ENDCOLOR}\n"
+clonacion_bash_catinfog
+printf "${GREEN}======================== ¡Clonados los archivos bash catinfog! ========================${ENDCOLOR}\n"
+
+printf "\n${BLUE}======================== Ejecutanto la caja de herramientas ========================${ENDCOLOR}\n"
 bash ~/bash/tools.sh
