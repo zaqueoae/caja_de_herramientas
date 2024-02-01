42bf515bdb570447130433dc93738f979df78945
Zaqueo Echeverria <zaqueoae@gmail.com>
2024-02-02 00:12:37 +0100

    Update gpg_local.sh

--- gpg_local.sh
+++ gpg_local.sh
@@ -16,8 +16,9 @@ mkdir -p llaves_backup
 rm -f llaves_backup/passwd.txt
 touch llaves_backup/passwd.txt
 passphrasse=$(openssl rand -base64 24)
-echo "$passphrasse"
 echo "$passphrasse" >> llaves_backup/passwd.txt
+passphrasse_subkeys=$(openssl rand -base64 24)
+echo "$passphrasse_subkeys" >> llaves_backup/passwd_subkeys.txt
 
 #Genero entropía
 sudo rngd -r /dev/urandom
@@ -26,7 +27,7 @@ sudo rngd -r /dev/urandom
 gpgconf --kill gpg-agent  # Required, if agent_genkey fail...
 gpgconf --kill gpg-agent  # Required, if agent_genkey fail...
 
-#Creo la llave privada y las subclaves
+#Creo la llave privada
 gpg --generate-key --batch <<eoGpgConf
     Key-Type: RSA
     Key-Length: 4096
@@ -36,63 +37,60 @@ gpg --generate-key --batch <<eoGpgConf
     Expire-Date: 9999-12-31
     Passphrase: $(<llaves_backup/passwd.txt)
     %commit
-    Subkey-Type: RSA
-    Subkey-Length: 4096
-    Subkey-Usage: sign
-    %commit
-    Subkey-Type: RSA
-    Subkey-Length: 4096
-    Subkey-Usage: encrypt
-    %commit
-    Subkey-Type: RSA
-    Subkey-Length: 4096
-    Subkey-Usage: auth
-    %commit
 eoGpgConf
 
 #Obtengo el id de la llave privada
 keyid=$(gpg --list-keys --keyid-format SHORT "$email" | grep pub | cut -d'/' -f2 | cut -d' ' -f1)
 
-#Exporto la llave privada y las subclaves para guardarlas a buen recaudo
+#Exporto la llave privada
 gpg --pinentry-mode loopback --passphrase "$(<llaves_backup/passwd.txt)" --output llaves_backup/privatekey.gpg --armor --export-secret-keys --export-options export-backup "$email"
-
-
-# Obtenemos las líneas que contienen información de subclaves
-subkey_lines=$(gpg --list-keys $keyid | awk '/sub/')
-
-while IFS= read -r line; do
-    # Extraemos el ID de la subclave y su uso
-    subkeyid=$(echo $line | awk '{print $2}' | cut -d'/' -f2)
-    usage=$(echo $line | awk '{print $1}')
-
-    # Determinamos el nombre del archivo basado en el uso de la subclave
-    filename=""
-    case $usage in
-        "ssb")
-            filename="sign"
-            ;;
-        "ssb>u")
-            filename="auth"
-            ;;
-        "ssb/e")
-            filename="encrypt"
-            ;;
-        *)
-            filename=$subkeyid  # si no podemos determinar el uso, usamos el ID de la subclave
-            ;;
-    esac
-
-    # Exportamos la subclave
-    gpg --output llaves_backup/subkey_${filename}.pgp --export-secret-subkeys ${subkeyid}!
-done <<< "$subkey_lines"
-
-
 
 #Exporto la llave pública
 gpg --output llaves_backup/publickey.gpg --armor --export "$email"
 
 #Envío la llave publica a un servidor publico
 #gpg --keyserver keyserver.ubuntu.com --send-keys "$keyid"
+
+#Obtengo la huella de la llave privada
+FPR=$(gpg --list-options show-only-fpr-mbox --list-secret-keys | awk '{print $1}')
+
+gpg --batch --passphrase '' \
+    --quick-add-key $FPR RSA sign 100y
+gpg --batch --passphrase '' \
+    --quick-add-key $FPR RSA encrypt 100y
+gpg --batch --passphrase '' \
+    --quick-add-key $FPR RSA auth 100y
+
+
+##########################
+#Exporto las subclaves para importarlas a un nuevo directorio temporal, cambiar la passphrasse y finalmente exportar esas subclaves
+##########################
+
+# Crea un directorio temporal para el nuevo anillo de claves
+tempdir2=$(mktemp -d)
+
+# Exporta las subclaves
+gpg --export-secret-subkeys "$keyid"! >llaves_backup/subkeys.pgp
+
+# Importa las subclaves al nuevo anillo de claves
+export GNUPGHOME=$tempdir2
+gpg --import llaves_backup/subkeys.pgp
+
+# Cambio la frase de contraseña de las subclaves
+echo "$passphrasse_subkeys" | gpg --command-fd 0 --edit-key "$keyid"
+
+# Exporto las subclaves
+gpg --export-secret-subkeys "$keyid"! >llaves_backup/new_subkeys.pgp
+
+# Elimina el directorio temporal y vuelve al directorio original
+rm -rf $tempdir2
+
+#Vuelvo al directorio gpg anterior
+export GNUPGHOME=$tempdir
+##########################
+#Fin exportación de subclaves
+##########################
+
 
 #Paro la generación de entropía
 pidrngd=$(pgrep rngd)
