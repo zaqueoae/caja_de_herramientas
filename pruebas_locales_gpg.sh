723d3a34f3b1933a2ee44c4cccae8c171afaa7d0
Zaqueo Echeverria <zaqueoae@gmail.com>
2024-02-02 21:05:56 +0100

    Update pruebas_locales_gpg.sh

--- pruebas_locales_gpg.sh
+++ pruebas_locales_gpg.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+unset GNUPGHOME
+rm -rf "$tempdir4"
+rm -rf "$tempdir5"
+
 #gpg variables. Modify this
 email="info@pacopepe3242335.com"
 
@@ -26,29 +30,35 @@ tempdir4=$(mktemp -d)
 # Configuo GNUPGHOME para apuntar al directorio temporal
 export GNUPGHOME="$tempdir4"
 
-#Me descargo la llave pública
-gpg --keyserver keyserver.ubuntu.com --trust-model always --recv-keys "$email"
-
-#Obtengo el id la llave publica
-publickeyid=$(gpg --list-keys "$email" | awk '/pub/{print $2}' | awk -F'/' '{print $2}')
-
-# Obtengo la huella digital de la clave pública
-public_key_fingerprint=$(gpg --fingerprint "$publickeyid" | grep -i fingerprint | awk '{print $2 $3 $4 $5 $6}')
-
 #Importo la subkey de firma
 echo $passphrase | gpg --batch --yes --passphrase-fd 0 --import llaves_backup/subkey_sign.pgp
 
-#Obtengo la huella digital de la subkey
-subkey_fingerprint=$(gpg --list-keys --with-subkey-fingerprint "$email" | awk '/sub/{getline; print}' | tail -1 | sed 's/ //g')
+#Saco la huella de la subkey
+huella_privada=$(gpg --fingerprint --with-colons "$email" | awk -F: '/fpr/{print $10}' | tr -d ' ')
+
+#Borro la subkey
+gpg --delete-secret-keys "$email"
+
+#Me descargo la llave pública
+gpg --keyserver keyserver.ubuntu.com --recv-keys "$email"
+#gpg --keyserver hkps://keyserver.ubuntu.com --with-colons --search-keys "$email"
+
+#Saco la huella de la llave publica
+huella_publica=$(gpg --fingerprint --with-colons "$email" | awk -F: '/fpr/{print $10}' | tr -d ' ')
 
 # Compara las huellas digitales
-if [ ! "$public_key_fingerprint" = "$subkey_fingerprint" ]; then
+if [ ! "$huella_publica" = "$huella_privada" ]; then
     echo "La clave pública no es auténtica"
     exit
+else
+    echo "La llave publica es auténtica"
 fi
 
+#Importo de nuevo la subkey de firma
+echo $passphrase | gpg --batch --yes --passphrase-fd 0 --import llaves_backup/subkey_sign.pgp
+
 #Obtengo el id de la subkey de firma
-subkeyid=$(gpg --list-secret-keys "$email" | awk '/ssb/{print $2}' | cut -d'/' -f2)
+subkeyid=$(gpg --list-keys --keyid-format SHORT "$email" | grep pub | cut -d'/' -f2 | cut -d' ' -f1)
 
 #Firmo
 echo $passphrase | gpg --batch --yes --passphrase-fd 0 --pinentry-mode loopback --local-user "$subkeyid" --output "${file_test}_sign.gpg" --sign "$file_test"
