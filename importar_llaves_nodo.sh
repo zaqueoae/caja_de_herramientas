483ec967a897bb58a0ceebda21f7243ef798d164
Zaqueo Echeverria <zaqueoae@gmail.com>
2024-02-04 20:16:41 +0100

    Create importar_llaves_nodo.sh

---
+++ importar_llaves_nodo.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+#Ejemplo de uso: gpg_importar_llave_publica mitia@g.com  bashcatinfog/llaves/llave_publica.pub bashcatinfog/llaves/subkey_sign.pgp
+gpg_importar_llave_publica () {
+
+email="$1"
+path_public_key="$2"
+path_private_key="$3"
+
+# Importa la clave principal y sus subclaves
+gpg --import "$path_public_key"
+
+# Obtén la huella digital de la clave principal
+fingerprint=$(gpg --with-colons --fingerprint "$email" | awk -F: '/fpr/ {print $10}')
+
+# Crea el archivo de confianza
+echo "${fingerprint}:6:" | gpg --import-ownertrust
+}
+gpg_importar_llave_privada(){
+# Pregunta por la passphrase de la subkey de firma
+read -r -s -p "Escribe la passphrase de la subkey de firma: " passphrase
+
+#Importo la subkey de firma
+echo $passphrase | gpg --batch --yes --passphrase-fd 0 --import "$path_private_key"
+}
+
+
+
+#Ejemplo de uso: comprime_firma_encripta /root/backup backup mitia@g.com
+comprime_firma_encripta(){
+folder="$1"
+file="$2"
+email="$3"
+# Comprime la carpeta
+tar -czf "$file".tar.gz "$folder"
+
+# Encripta y firma el archivo
+gpg --recipient "$email" --local-user "$email" --output "$file".tar.gz.gpg --encrypt --sign "$file".tar.gz
+
+#Dejo el archivo en la carpeta del usuriao orquestador para que se la descargue el bastion
+cp "$file".tar.gz.gpg /home/orquestador/
+chown orquestador:orquestador /home/orquestador/"$file".tar.gz.gpg
+
+#Borro el directorio y el comprimido
+rm -rf "$folder"
+rm -f "$file".tar.gz
+}
